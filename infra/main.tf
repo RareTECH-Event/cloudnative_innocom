bcac869e7649d7862a641244ffa948cd8d488eaf
pond-village <r-ikemura@var.co.jp>
2025-07-16 08:14:53 +0900

    feat: セキュリティグループ(ALB, ECSタスク)の追加

--- infra/main.tf
+++ infra/main.tf
@@ -138,3 +138,50 @@ resource "aws_route_table_association" "private_1c" {
 resource "aws_ecs_cluster" "main" {
   name = "main-cluster"
 }
+
+# Security Group
+resource "aws_security_group" "alb" {
+  name   = "alb-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+resource "aws_security_group" "ecs_task" {
+  name   = "ecs-task-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecs-task-sg"
+  }
+}
