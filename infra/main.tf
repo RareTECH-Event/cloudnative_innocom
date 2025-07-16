2085ab0dd644d6d306ea7ea37a80ea5b31e02137
pond-village <r-ikemura@var.co.jp>
2025-07-16 09:16:03 +0900

    fix: ECSタスクのセキュリティグループが5000番ポートを許可するよう修正

--- infra/main.tf
+++ infra/main.tf
@@ -168,8 +168,8 @@ resource "aws_security_group" "ecs_task" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port       = 80
-    to_port         = 80
+    from_port       = 5000
+    to_port         = 5000
     protocol        = "tcp"
     security_groups = [aws_security_group.alb.id]
   }
